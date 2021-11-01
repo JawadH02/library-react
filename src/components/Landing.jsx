8d691fb41d0fb8a4cb5bf8ba5c6bfba3c93a5bad
JawadHanif <JawadHanif.002@gmail.com>
2021-11-01 15:24:40 -0400

    compressed images

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import UndrawBooks from '../assets/Undraw_Books.svg';
 
 const Landing = () => {
@@ -10,9 +9,9 @@ const Landing = () => {
                     <div className="header__description">
                         <h1>America's most awarded online streaming platform</h1>
                         <h2>Find your dream show with <span className="white">Library</span></h2>
-                        <Link to="#features">
+                        <a href="#features">
                             <button className="btn">Browse shows</button>
-                        </Link>
+                        </a>
                     </div>
                     <figure className="header__img--wrapper">
                         <img src={UndrawBooks} alt="" />
